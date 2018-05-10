3198626620b12cfa3b8e0e01306bc530e48866a3
karim <karim@udacity.com>
2018-05-10 14:52:53 -0700

    Update graphing

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RoboND-PathPlanningQuizzes
 Path Planning Quizzes
+```sh
+$ g++ minkowski_sum.cpp -o app -std=c++11 -I/usr/include/python2.7 -lpython2.7
+```
--- minkowski_sum.cpp
+++ minkowski_sum.cpp
@@ -1,4 +1,3 @@
-// Compile with: g++ minkowski_sum.cpp -o app -std=c++11 -I/usr/include/python2.7 -lpython2.7
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -7,7 +6,7 @@
 using namespace std;
 namespace plt = matplotlibcpp;
 
-// Print 2D Vectors
+// Print 2D vectors coordinate values
 void print2DVector(vector<vector<int> > vec)
 {
     for (int i = 0; i < vec.size(); ++i) {
@@ -22,7 +21,7 @@ void print2DVector(vector<vector<int> > vec)
     }
 }
 
-// Check for duplicates inside a 2D vector and delete them
+// Check for duplicate inside a 2D vector and delete them
 vector<vector<int> > delete_duplicate(vector<vector<int> > C)
 {
     // Sort the C vector
@@ -55,8 +54,8 @@ vector<vector<int> > minkowski_sum(vector<vector<int> > A, vector<vector<int> > 
     return C;
 }
 
-// Draw the coordinates
-void draw_coordinates(vector<vector<int> > A, vector<vector<int> > B, vector<vector<int> > C)
+// Draw A, B and C shapes
+void draw_shapes()
 {
     //Graph Format
     plt::title("Minkowski Sum");
@@ -64,85 +63,75 @@ void draw_coordinates(vector<vector<int> > A, vector<vector<int> > B, vector<vec
     plt::ylim(-5, 5);
     plt::grid(true);
 
-    // Draw A vector coordinates
-    for (double x = 0; x <= 1; x += 0.01) {
-        for (double y = -1; y <= 1; y += 0.01) {
-            if (abs(y + x - 1) < 0.01 || abs(y - x + 1) < 0.01 || x <= 0.01) {
-                plt::plot({ x }, { y }, "b.");
-            }
-        }
-    }
+    // Define precision for floating point values
+    double prec = 0.01;
 
-    // Draw B vector coordinates
-    for (double x = 0; x <= 1; x += 0.01) {
-        for (double y = -1; y <= 1; y += 0.01) {
-            if (abs(y - x) < 0.01 || abs(y + x) < 0.01 || abs(x - 1) <= 0.01) {
-                plt::plot({ x }, { y }, "r.");
+    // Loop through all the pixels and draw the shapes
+    for (double x = 0; x <= 2; x += 0.01) {
+        for (double y = -2; y <= 2; y += 0.01) {
+            // Draw A shape
+            if (x >= 0 && x <= 1 && y >= -1 && y <= 1) {
+                if (abs(y + x - 1) < prec || abs(y - x + 1) < prec || x <= prec)
+                    plt::plot({ x }, { y }, "b.");
             }
-        }
-    }
+            // Draw B shape
+            if (x >= 0 && x <= 1 && y >= -1 && y <= 1) {
+                if (abs(y - x) < 0.01 || abs(y + x) < 0.01 || abs(x - 1) <= 0.01)
+                    plt::plot({ x }, { y }, "r.");
+            }
+            // Draw C shape
+            if (y >= -1 && y <= 1) {
+                plt::plot({ 0 }, { y }, "g.");
+            }
 
-    // Draw C vector coordinates
-    for (double y = -1; y <= 1; y += 0.01) {
-        plt::plot({ 0 }, { y }, "g.");
-        plt::plot({ 2 }, { y }, "g.");
-    }
+            if (y >= -1 && y <= 1) {
+                plt::plot({ 2 }, { y }, "g.");
+            }
 
-    for (double x = 0; x <= 1; x += 0.01) {
-        for (double y = 1; y <= 2; y += 0.01) {
-            if (abs(y - x - 1) < 0.01) {
-                plt::plot({ x }, { y }, "g.");
+            if (x >= 0 && x <= 1 && y >= 1 && y <= 2) {
+                if (abs(y - x - 1) < 0.01)
+                    plt::plot({ x }, { y }, "g.");
             }
-        }
-    }
-    for (double x = 0; x <= 1; x += 0.01) {
-        for (double y = -2; y <= -1; y += 0.01) {
-            if (abs(y + x + 1) < 0.01) {
-                plt::plot({ x }, { y }, "g.");
+            if (x >= 0 && x <= 1 && y >= -2 && y <= -1) {
+                if (abs(y + x + 1) < 0.01)
+                    plt::plot({ x }, { y }, "g.");
             }
-        }
-    }
-    for (double x = 1; x <= 2; x += 0.01) {
-        for (double y = 1; y <= 2; y += 0.01) {
-            if (abs(y + x - 3) < 0.01) {
-                plt::plot({ x }, { y }, "g.");
+            if (x >= 1 && x <= 2 && y >= 1 && y <= 2) {
+                if (abs(y + x - 3) < 0.01)
+                    plt::plot({ x }, { y }, "g.");
             }
-        }
-    }
-    for (double x = 1; x <= 2; x += 0.01) {
-        for (double y = -2; y <= -1; y += 0.01) {
-            if (abs(y - x + 3) < 0.01) {
-                plt::plot({ x }, { y }, "g.");
+            if (x >= 1 && x <= 2 && y >= -2 && y <= -1) {
+                if (abs(y - x + 3) < 0.01)
+                    plt::plot({ x }, { y }, "g.");
             }
+            
         }
     }
 
     //Save the image and close the plot
-    plt::save("./Images/Minkowski.png");
+    plt::save("./Images/Minkowski_Sum.png");
     plt::clf();
 }
 
 int main()
 {
-    // Define the edges of each triangle
+    // Define the coordinates of triangle A and B in 2D vectors
     vector<vector<int> > A(3, vector<int>(2));
-    A = {
-        { 1, 0 }, { 0, 1 }, { 0, -1 },
-    };
+    A = {{ 1, 0 }, { 0, 1 }, { 0, -1 },};
     vector<vector<int> > B(3, vector<int>(2));
     B = {
         { 0, 0 }, { 1, 1 }, { 1, -1 },
     };
 
-    // Compute the minkowski sum of the two triangles
+    // Compute the minkowski sum of triangle A and B
     vector<vector<int> > C;
     C = minkowski_sum(A, B);
 
     // Print the resulting vector
     print2DVector(C);
 
-    // Plot the coordinates of each object
-    draw_coordinates(A, B, C);
+    // Draw all the shapes 
+    draw_shapes();
 
     return 0;
 }
