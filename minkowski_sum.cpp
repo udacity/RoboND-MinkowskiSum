a2a1b3b98723ca03958e352fd80b1b1088d388e4
karim <karim@udacity.com>
2018-05-10 17:26:09 -0700

    Updated Graphics

--- minkowski_sum.cpp
+++ minkowski_sum.cpp
@@ -7,7 +7,7 @@ using namespace std;
 namespace plt = matplotlibcpp;
 
 // Print 2D vectors coordinate values
-void print2DVector(vector<vector<int> > vec)
+void print2DVector(vector<vector<double> > vec)
 {
     for (int i = 0; i < vec.size(); ++i) {
         cout << "(";
@@ -22,12 +22,12 @@ void print2DVector(vector<vector<int> > vec)
 }
 
 // Check for duplicate inside a 2D vector and delete them
-vector<vector<int> > delete_duplicate(vector<vector<int> > C)
+vector<vector<double> > delete_duplicate(vector<vector<double> > C)
 {
     // Sort the C vector
     sort(C.begin(), C.end());
     // Initialize a non duplicated vector
-    vector<vector<int> > Cn;
+    vector<vector<double> > Cn;
     for (int i = 0; i < C.size() - 1; i++) {
         // Check if it's a duplicate coordinate
         if (C[i] != C[i + 1]) {
@@ -39,13 +39,13 @@ vector<vector<int> > delete_duplicate(vector<vector<int> > C)
 }
 
 // Compute the minkowski sum of two vectors
-vector<vector<int> > minkowski_sum(vector<vector<int> > A, vector<vector<int> > B)
+vector<vector<double> > minkowski_sum(vector<vector<double> > A, vector<vector<double> > B)
 {
-    vector<vector<int> > C;
+    vector<vector<double> > C;
     for (int i = 0; i < A.size(); i++) {
         for (int j = 0; j < B.size(); j++) {
             // Compute the current sum
-            vector<int> Ci = { A[i][0] + B[j][0], A[i][1] + B[j][1] };
+            vector<double> Ci = { A[i][0] + B[j][0], A[i][1] + B[j][1] };
             // Push it to the C vector
             C.push_back(Ci);
         }
@@ -54,8 +54,45 @@ vector<vector<int> > minkowski_sum(vector<vector<int> > A, vector<vector<int> > 
     return C;
 }
 
+// Workout the combination: http://rosettacode.org/wiki/Combinations#C.2B.2B
+vector<vector<double> > generate_combinations(int N)
+{
+    int K = 2;
+    vector<vector<double> > comb;
+    std::string bitmask(K, 1); // K leading 1's
+    bitmask.resize(N, 0); // N-K trailing 0's
+
+    // print integers and permute bitmask
+    do {
+        vector<double> combi = { 0, 0 };
+        int j = 0;
+        for (int i = 0; i < N; ++i) // [0..N-1] integers
+        {
+            if (bitmask[i]) {
+                combi[j] = i;
+                j++;
+            }
+        }
+        comb.push_back(combi);
+    } while (std::prev_permutation(bitmask.begin(), bitmask.end()));
+
+    return comb;
+}
+
+// Process the combinations of a shape and plot its coordinates
+void plot(vector<vector<double> > vec, string color)
+{
+
+    vector<vector<double> > comb;
+    comb = generate_combinations(vec.size());
+    for (int i = 0; i < comb.size(); i++) {
+        if (abs(vec[comb[i][0]][1] - vec[comb[i][1]][1]) <= 2 && abs(vec[comb[i][0]][0] - vec[comb[i][1]][0]) <= 1)
+            plt::plot({ vec[comb[i][0]][0], vec[comb[i][1]][0] }, { vec[comb[i][0]][1], vec[comb[i][1]][1] }, color);
+    }
+}
+
 // Draw A, B and C shapes
-void draw_shapes()
+void draw_shapes(vector<vector<double> > A, vector<vector<double> > B, vector<vector<double> > C)
 {
     //Graph Format
     plt::title("Minkowski Sum");
@@ -63,50 +100,14 @@ void draw_shapes()
     plt::ylim(-5, 5);
     plt::grid(true);
 
-    // Define precision for floating point values
-    double prec = 0.01;
+    // Draw configuration space C
+    plot(C, "g-");
 
-    // Loop through all the pixels and draw the shapes
-    for (double x = 0; x <= 2; x += 0.01) {
-        for (double y = -2; y <= 2; y += 0.01) {
-            // Draw A shape
-            if (x >= 0 && x <= 1 && y >= -1 && y <= 1) {
-                if (abs(y + x - 1) < prec || abs(y - x + 1) < prec || x <= prec)
-                    plt::plot({ x }, { y }, "b.");
-            }
-            // Draw B shape
-            if (x >= 0 && x <= 1 && y >= -1 && y <= 1) {
-                if (abs(y - x) < 0.01 || abs(y + x) < 0.01 || abs(x - 1) <= 0.01)
-                    plt::plot({ x }, { y }, "r.");
-            }
-            // Draw C shape
-            if (y >= -1 && y <= 1) {
-                plt::plot({ 0 }, { y }, "g.");
-            }
+    // Draw triangle A
+    plot(A, "b-");
 
-            if (y >= -1 && y <= 1) {
-                plt::plot({ 2 }, { y }, "g.");
-            }
-
-            if (x >= 0 && x <= 1 && y >= 1 && y <= 2) {
-                if (abs(y - x - 1) < 0.01)
-                    plt::plot({ x }, { y }, "g.");
-            }
-            if (x >= 0 && x <= 1 && y >= -2 && y <= -1) {
-                if (abs(y + x + 1) < 0.01)
-                    plt::plot({ x }, { y }, "g.");
-            }
-            if (x >= 1 && x <= 2 && y >= 1 && y <= 2) {
-                if (abs(y + x - 3) < 0.01)
-                    plt::plot({ x }, { y }, "g.");
-            }
-            if (x >= 1 && x <= 2 && y >= -2 && y <= -1) {
-                if (abs(y - x + 3) < 0.01)
-                    plt::plot({ x }, { y }, "g.");
-            }
-            
-        }
-    }
+    // Draw triangle B
+    plot(B, "r-");
 
     //Save the image and close the plot
     plt::save("./Images/Minkowski_Sum.png");
@@ -116,20 +117,24 @@ void draw_shapes()
 int main()
 {
     // Define the coordinates of triangle A and B in 2D vectors
-    vector<vector<int> > A(3, vector<int>(2));
-    A = {{ 1, 0 }, { 0, 1 }, { 0, -1 },};
-    vector<vector<int> > B(3, vector<int>(2));
-    B = {{ 0, 0 }, { 1, 1 }, { 1, -1 },};
+    vector<vector<double> > A(3, vector<double>(2));
+    A = {
+        { 1, 0 }, { 0, 1 }, { 0, -1 },
+    };
+    vector<vector<double> > B(3, vector<double>(2));
+    B = {
+        { 0, 0 }, { 1, 1 }, { 1, -1 },
+    };
 
-    // Compute the minkowski sum of triangle A and B
-    vector<vector<int> > C;
+    // Compute the Minkowski Sum of triangle A and B
+    vector<vector<double> > C;
     C = minkowski_sum(A, B);
 
     // Print the resulting vector
     print2DVector(C);
 
-    // Draw all the shapes 
-    draw_shapes();
+    // Draw all the shapes
+    draw_shapes(A, B, C);
 
     return 0;
 }
